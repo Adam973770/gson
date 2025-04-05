25c1a7ceed6f97c5a7fae31c98ac7c799eced544
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2025-04-05 17:21:21 +0200

    interface de la strategie de serialisation

--- gson/src/main/java/com/google/gson/DefaultLongSerializationStrategy.java
+++ gson/src/main/java/com/google/gson/DefaultLongSerializationStrategy.java
@@ -1,1 +1,11 @@
+package com.google.gson;
 
+public class DefaultLongSerializationStrategy implements LongSerializationStrategy {
+  @Override
+  public JsonElement serialize(Long value) {
+    if (value == null) {
+      return JsonNull.INSTANCE;
+    }
+    return new JsonPrimitive(value);
+  }
+}
