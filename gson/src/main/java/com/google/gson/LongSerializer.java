05594f42c5412c4a8fc5cbbe439f2f6c5bee857d
Adam RAMJATTAN <adam.ramjattan.etu@univ-lille.fr>
2025-04-05 17:29:50 +0200

    classes contenant les methodes permettant de définir qu'elle stratégie de sérialisation vas être employé

--- gson/src/main/java/com/google/gson/LongSerializer.java
+++ gson/src/main/java/com/google/gson/LongSerializer.java
@@ -1,1 +1,18 @@
+package com.google.gson;
 
+public class LongSerializer {
+
+  private LongSerializationStrategy strategy;
+
+  public LongSerializer(LongSerializationStrategy strategy) {
+    this.strategy = strategy;
+  }
+
+  public void setSerializationStrategy(LongSerializationStrategy strategy) {
+    this.strategy = strategy;
+  }
+
+  public JsonElement serialize(Long value) {
+    return strategy.serialize(value);
+  }
+}
